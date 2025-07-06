95b17173554126b7d7cecb8f545303d25393660a
Chris-80 <chrisguy450@gmail.com>
2025-07-06 04:53:59 -0400

    Consolidated duplicate code in to one method 'isButtonOnNew()' to replace the checkS1,2,3. Added logic for enabling/disabling serial debug output based on a ping being pulled low.

--- src/BinaryClock_ESP32.cpp
+++ src/BinaryClock_ESP32.cpp
@@ -8,6 +8,17 @@ using namespace BinaryClockShield;
 
 void setup()
    {
+   Serial.begin(115200); // Start the serial communication at 115200 baud rate
+//   BUILTIN_LED = 2; // Set the built-in LED pin to 2 for ESP32
+   pinMode(LED_BUILTIN, OUTPUT); // Initialize the built-in LED pin as an output
+   for (int i = 0; i < 5; i++)
+      {
+      digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
+      delay(250);
+      digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
+      delay(250);
+      }
+
    // put your setup code here, to run once:
    BinaryClock::get_Instance().setup();
    }
